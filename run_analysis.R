138309954ed8ee922be5d39cfe5b79b07d9b42fc
Brian Bai <tianxiangbai@gmail.com>
2016-05-07 16:41:45 +0800

    Update R script according to the comments

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,6 @@
 ##This R script includes R codes for JHU Getting
 ##and cleaning data course project
-library(reshape2)
+library(dplyr)
 
 ##Download and load data
 filename <- "getdata_dataset.zip"
@@ -22,7 +22,7 @@ features <- read.table(".//UCI HAR Dataset//features.txt")
 activityLabels <- read.table(".//UCI HAR Dataset//activity_labels.txt")
 
 ##Extract the feature of interest (mean & standard deviation)
-feature.index <- grep(".*mean.*|.*std.*", features[,2])
+feature.index <- grep("mean\\(\\)|std\\(\\)", features[,2])
 feature.name <- features[feature.index,2]
 feature.name <- gsub("-","",feature.name)
 feature.name <- gsub('[()]','',feature.name)
@@ -34,17 +34,27 @@ testData <- testData[,feature.index]
 trainData <- cbind(trainSubjects,trainLabels,trainData)
 testData <- cbind(testSubjects,testLabels,testData)
 MergedData <- rbind(trainData,testData)
-colnames(MergedData) <- tolower(c("subjects","activities",feature.name))
+colnames(MergedData) <- c("Subjects","Activities",feature.name)
 
-##Name the activities in the data set
-MergedData$activities <- factor(MergedData$activities, levels = activityLabels[,1],labels = tolower(activityLabels[,2]))
+##Uses descriptive activity names to name the activities in the data set
+MergedData$Activities <- factor(MergedData$Activities, levels = activityLabels[,1],labels = tolower(activityLabels[,2]))
 
 ##Turn the subjects and activities columns into factor
-MergedData$subjects <- as.factor(MergedData$subjects)
+MergedData$Subjects <- as.factor(MergedData$Subjects)
+
+##Appropriately labels the data set with descriptive variable names
+names(MergedData)<-gsub("^t", "Time", names(MergedData))
+names(MergedData)<-gsub("^f", "Frequency", names(MergedData))
+names(MergedData)<-gsub("Acc", "Accelerometer", names(MergedData))
+names(MergedData)<-gsub("Gyro", "Gyroscope", names(MergedData))
+names(MergedData)<-gsub("Mag", "Magnitude", names(MergedData))
+names(MergedData)<-gsub("BodyBody", "Body", names(MergedData))
+names(MergedData)<-gsub("mean", "Mean", names(MergedData))
+names(MergedData)<-gsub("std", "Std", names(MergedData))
 
 ##Create a second, independent tidy data set with the average 
 ##of each variable for each activity and each subject.
-MergedData.melted <- melt(MergedData, id = c("subjects", "activities"))
-MergedData.mean <- dcast(MergedData.melted, subjects + activities ~ variable, mean)
+MergedData.aggr <- aggregate(. ~ Subjects - Activities, data = MergedData, mean)
+MergedData.mean <- tbl_df(arrange(MergedData.aggr,Subjects,Activities))
 
 write.table(MergedData.mean, "tidy.txt", row.names = FALSE, quote = FALSE)
